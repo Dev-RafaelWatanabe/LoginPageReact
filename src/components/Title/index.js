3ae6782ddd2e56683894194b2550487a8e12935b
Rafael Watanabe <rafaelwatanabe2006@gmail.com>
2021-10-17 21:04:13 -0400

    in progress

---
+++ src/components/Title/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    color: white;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: bold;
+    font-size: 36px;
+    text-align: center;
+    text-transform: uppercase;
+`;
+
+export default Title;
---
+++ src/styles/elements/Base.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const Base = createGlobalStyle `
+    body {
+        background-color: #2148C0;
+    }
+`;
+
+export default Base
